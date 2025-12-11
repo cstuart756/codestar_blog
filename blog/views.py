969926e81fb4153991f92d6a41d8484e79af6101
cstuart756 <stujjc@yahoo.co.uk>
2025-12-11 17:39:52 +0000

    Fix blog URL: add blog_home view, /blog/ path, deployment-ready settings

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
 
+# Create your view for the blog home
 def blog_home(request):
     return HttpResponse("Hello, blog!")
--- codestar/urls.py
+++ codestar/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog import views as blog_views
+from blog import views as blog_views  # import your blog views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog_views.blog_home, name='blog_home'),
+    path('blog/', blog_views.blog_home, name='blog_home'),  # /blog/ URL
 ]
+
 
 
